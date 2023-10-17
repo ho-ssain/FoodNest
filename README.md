679fe7523eaf8a8d05664b621880bd2b0e02af8c
Hossain K. M. <awarehossain@gmail.com>
2023-10-18 00:27:38 +0600

    phase-2

---
+++ README.md
@@ -0,0 +1,26 @@
+# BurgerNest
+
+## (An e-commerce online food-ordering system for selling burgers and various other food items.)
+
+### Implementation overview:-
+
+**Used Stack**
+MEAN (MongoDB, Express, Angular, Node)
+
+**_Environment tools-:_**
+
+- git
+- github
+- VScode
+- Angular CLI
+- nodeJS
+- npm
+- RapidAPI Client
+- required VScode extensions for this project
+
+**_Client side creation Phase-:_**
+
+- npm i -g @angular/cli
+- ng version
+- ng n frontend
+- ng serve -o
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,50 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/partials/header/header.component';
-import { HomeComponent } from './components/pages/home/home.component';
-import { StarRatingComponent } from './components/partials/star-rating/star-rating.component';
-import { SearchComponent } from './components/partials/search/search.component';
-import { TagsComponent } from './components/partials/tags/tags.component';
-import { FoodPageComponent } from './components/pages/food-page/food-page.component';
-import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
-import { TitleComponent } from './components/partials/title/title.component';
-import { NotFoundComponent } from './components/partials/not-found/not-found.component';
-import { LoginPageComponent } from './components/pages/login-page/login-page.component';
-import { TextInputComponent } from './components/partials/text-input/text-input.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { DefaultButtonComponent } from './components/partials/default-button/default-button.component';
-import { InputContainerComponent } from './components/partials/input-container/input-container.component';
-import { InputValidationComponent } from './components/partials/input-validation/input-validation.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    StarRatingComponent,
-    SearchComponent,
-    TagsComponent,
-    FoodPageComponent,
-    CartPageComponent,
-    TitleComponent,
-    NotFoundComponent,
-    LoginPageComponent,
-    TextInputComponent,
-    DefaultButtonComponent,
-    InputContainerComponent,
-    InputValidationComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
